530071f404c3bdfad05502e07e1645fc4580d702
Josh Turman <jturman18@icloud.com>
2020-03-12 12:01:24 -0700

    Halfway done with Question 4 Unit 3

--- U3_Question4.java
+++ U3_Question4.java
@@ -13,7 +13,34 @@ public class U3_Question4
         
     }
 
-    public void go()
+    public void go_option1()
+    {
+        System.out.println("\n\nQuestion4: ");
+        
+        int x = 8 , y = 6;
+        
+        // This method will output the difference of the ints x and y.
+        // It checks to see which of the ints is larger, and the subtracts
+        // the smaller one from the larger one.
+        // If the ints are the same, it will just print out 0.
+        int result = 0;
+        if (x > y)
+        {
+            result = x - y;
+            System.out.print(result);
+        }
+        else if (x < y)
+        {
+            result = y - x;
+            System.out.print(result);
+        }
+        else
+        {
+            System.out.print(result);
+        }
+    }
+    
+    public void go_option2()
     {
         
     }
