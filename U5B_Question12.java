d83f75f08103be47878fe60e3771273acaaa4b2a
Josh Turman <42820729+Josh-Turman18@users.noreply.github.com>
2020-03-12 18:01:18 -0700

    added the framework for question 12

---
+++ U5B_Question12.java
@@ -0,0 +1,21 @@
+/**
+ * Executes Unit 5B Question 12 and explains the solution
+ *
+ * @Josh Turman
+ * @1.0
+ */
+public class U5B_Question12
+{
+    
+    public U5B_Question12()
+    {
+        
+    }
+
+    public void go()
+    {
+        System.out.println("\nQuestion 12: ");
+        
+        
+    }
+}
