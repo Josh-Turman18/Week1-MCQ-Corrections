b05c85d3b64dec500ec31bceaa9f9c4213074301
Josh Turman <42820729+Josh-Turman18@users.noreply.github.com>
2020-03-12 17:31:04 -0700

    finished question 6

--- U3_Question6.java
+++ U3_Question6.java
@@ -15,6 +15,28 @@ public class U3_Question6
 
     public void go()
     {
+        System.out.println("\nQuestion 6:");
         
+        int j = 4, k = 4, int m = 8;
+        
+        if (!((j == k) && (k > m)))     // This is the conditional in the question
+        // This compound condtional results in a true statement
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+            
+        if ((j != k) || (k <= m))   // This is the conditional of the correct answer
+        // (answer A), this compound conditional also results in a true statement
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
     }
 }
