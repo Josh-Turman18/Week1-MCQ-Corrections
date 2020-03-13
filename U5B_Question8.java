36277f3922c6103b48dfc18bf7ad0814395c11a8
Josh Turman <42820729+Josh-Turman18@users.noreply.github.com>
2020-03-12 23:26:16 -0700

    finished the code for this class

--- U5B_Question8.java
+++ U5B_Question8.java
@@ -16,6 +16,14 @@ public class U5B_Question8
     {
         System.out.println("\nQuestion 8: ");
         
+        // This is the original (broken) code
+        System.out.println("Nothing is printed with the original code,");
+        Student s = new Student("Priya", "Banerjee", -1);
+        System.out.println(s);
         
+        // This is one way to fix the code
+        System.out.println("but when the error is fixed, this is what's printed:");
+        Student2 s2 = new Student2("Priya", "Banerjee", -1);
+        System.out.println(s2);
     }
 }
