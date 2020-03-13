10d8b8ec7a89f603ea5659ac3d28dac87e55b657
Josh Turman <42820729+Josh-Turman18@users.noreply.github.com>
2020-03-12 17:58:02 -0700

    added the framework for question 5

---
+++ U5B_Question5.java
@@ -0,0 +1,21 @@
+/**
+ * Executes Unit 5B Question 5 and explains the solution
+ *
+ * @Josh Turman
+ * @1.0
+ */
+public class U5B_Question5
+{
+    
+    public U5B_Question5()
+    {
+        
+    }
+
+    public void go()
+    {
+        System.out.println("\nQuestion 5: ");
+        
+        
+    }
+}
