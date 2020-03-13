32f5ff95de6e4650251395b8580c8be995225465
Josh Turman <42820729+Josh-Turman18@users.noreply.github.com>
2020-03-12 23:02:25 -0700

    added an output

--- U5B_Question4.java
+++ U5B_Question4.java
@@ -20,5 +20,7 @@ public class U5B_Question4
         Gadget.setStatus(3);  // changes the status value of Gadget to 3
         Gadget b = new Gadget();  // creates the object b and changes the value of
         // status back to 10, thus the correct answer is C
+        
+        System.out.println("No code is printed out for this question\nit just creates objects that contain a static variable");
     }
 }
