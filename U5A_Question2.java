1fb34f5edbdd28989727a6ee11cab14219156185
Josh Turman <42820729+Josh-Turman18@users.noreply.github.com>
2020-03-12 17:46:48 -0700

    added the framework for unit 5A question 2

---
+++ U5A_Question2.java
@@ -0,0 +1,21 @@
+/**
+ * Executes Unit 5A Question 2 and explains the solution
+ *
+ * @Josh Turman
+ * @1.0
+ */
+public class U5A_Question2
+{
+    
+    public U5A_Question2()
+    {
+        
+    }
+
+    public void go()
+    {
+        System.out.println("\nQuestion 2: ");
+        
+        
+    }
+}
